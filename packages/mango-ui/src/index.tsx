f17387167ba209c44be3e78c8329527dbd21823a
udielenberg <udielenberg@gmail.com>
2021-05-23 11:20:03 +0300

    update mango

--- packages/mango-ui/src/index.tsx
+++ packages/mango-ui/src/index.tsx
@@ -1,9 +1,70 @@
-import React, { InputHTMLAttributes } from "react";
-// interface Props extends InputHTMLAttributes<HTMLInputElement> {
-interface Props extends InputHTMLAttributes<any> {
-	color: string;
-	style?: React.CSSProperties;
+import styled from "styled-components";
+
+export { Global } from "./common/global";
+
+export const TextInput = styled.input`
+	width: 130px;
+	box-sizing: border-box;
+	border-radius: 4px;
+	font-size: 16px;
+	background-color: white;
+	background-image: url("searchicon.png");
+	background-position: 10px 10px;
+	background-repeat: no-repeat;
+	padding: 12px 20px 12px 40px;
+	transition: width 0.4s ease-in-out;
+	border: 3px solid ${({ color }: { color: string }) => color};
+	transition: width 0.4s ease-in-out;
+	outline-color: ${(props) => props.color};
+
+	&:focus {
+		width: 100%;
+	}
+`;
+
+export const Button = styled.div<any>`
+	padding: 5px 10px;
+	border-radius: 10px;
+	background: ${(props) => (props.different ? "#4148b3" : "#c040c0")};
+	color: white;
+	width: fit-content;
+`;
+
+const BlockContainer = styled.div`
+	.container {
+		display: flex;
+		align-items: baseline;
+	}
+
+	.block-label {
+		font-weight: bold;
+		text-transform: uppercase;
+		margin-right: 10px;
+	}
+
+	pre {
+		padding: 10px;
+		background: pink;
+		font-size: 1.2rem;
+
+		white-space: pre-wrap; /* Since CSS 2.1 */
+		white-space: -moz-pre-wrap; /* Mozilla, since 1999 */
+		white-space: -pre-wrap; /* Opera 4-6 */
+		white-space: -o-pre-wrap; /* Opera 7 */
+		word-wrap: break-word;
+	}
+`;
+
+interface BlockComponentProps {
+	label: string;
+	value: string;
 }
-export const TextInput: React.FC<Props> = ({ color, ...props }) => {
-	return <input {...props} style={{ border: `3px solid ${color}` }} />;
+
+export const Block: React.FC<BlockComponentProps> = (props) => {
+	return (
+		<BlockContainer>
+			<span className="block-label">{props.label}:</span>
+			<pre>{props.value}</pre>
+		</BlockContainer>
+	);
 };
